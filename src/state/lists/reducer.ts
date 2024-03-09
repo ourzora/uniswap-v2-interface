78b9da290c2da2f322f9711e0a33df74cff2934d
iain nash <me@iain.in>
2024-03-09 00:37:55 -0500

    update zora tokenlist

--- src/state/lists/reducer.ts
+++ src/state/lists/reducer.ts
@@ -4,7 +4,8 @@ import { TokenList } from '@uniswap/token-lists/dist/types'
 import { DEFAULT_LIST_OF_LISTS, DEFAULT_TOKEN_LIST_URL } from '../../constants/lists'
 import { updateVersion } from '../global/actions'
 import { acceptListUpdate, addList, fetchTokenList, removeList, selectList } from './actions'
-import UNISWAP_DEFAULT_LIST from '@uniswap/default-token-list'
+// import UNISWAP_DEFAULT_LIST from '@uniswap/default-token-list'
+import ZORA_DEFAULT_LIST from '../../constants/token-lists/zora.json';
 
 export interface ListsState {
   readonly byUrl: {
@@ -38,7 +39,8 @@ const initialState: ListsState = {
     }, {}),
     [DEFAULT_TOKEN_LIST_URL]: {
       error: null,
-      current: UNISWAP_DEFAULT_LIST,
+      current: ZORA_DEFAULT_LIST,
+
       loadingRequestId: null,
       pendingUpdate: null
     }
